bf0db98ab1316602070f8f7984f059b72137773c
Leo Crestel Mac <crestel.leopold@gmail.com>
2016-10-13 15:03:41 +0200

    default argument for local d3.js path in the visualization script. Platform independent :) Starting working on cRnnRbm, almost finished

--- acidano/visualization/numpy_array/write_numpy_array_html.py
+++ acidano/visualization/numpy_array/write_numpy_array_html.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import os
+import re
 
-def write_numpy_array_html(filename, dataname, d3js_source_path):
+
+def write_numpy_array_html(filename, dataname, d3js_source_path=None):
+    if d3js_source_path is None:
+        user_paths = os.environ['PYTHONPATH'].split(os.pathsep)
+        for user_path in user_paths:
+            if re.search('acidano', user_path):
+                d3js_source_path = user_path + 'acidano/visualization/d3.v3.min.js'
+                break
+
     text = """
 <!DOCTYPE html>
 <meta charset="utf-8">
