000b23731ac150226f7a3eb17d7dc6f964b71359
leo crestel <leopold.crestel@ircam.fr>
2016-12-13 12:10:34 -0500

    Deals with old hyperopt version avail on Guillimin

--- acidano/utils/hopt_wrapper.py
+++ acidano/utils/hopt_wrapper.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import hyperopt
 
-from hyperopt.pyll_utils import validate_label
+print(hyperopt.version.version)
+import hyperopt.pyll_utils as pyll_utils
 from hyperopt.pyll import scope
 
-@validate_label
-def qloguniform_int(label, *args, **kwargs):
-    return scope.int(
-        scope.hyperopt_param(label,
-                             scope.qloguniform(*args, **kwargs)))
+if 'validate_label' in dir(pyll_utils):
+    @validate_label
+    def qloguniform_int(label, *args, **kwargs):
+        return scope.int(
+            scope.hyperopt_param(label,
+                                 scope.qloguniform(*args, **kwargs)))
 
-@validate_label
-def quniform_int(label, *args, **kwargs):
-    return scope.int(
-        scope.hyperopt_param(label,
-                             scope.quniform(*args, **kwargs)))
+    @validate_label
+    def quniform_int(label, *args, **kwargs):
+        return scope.int(
+            scope.hyperopt_param(label,
+                                 scope.quniform(*args, **kwargs)))
+else:
+    def qloguniform_int(label, *args, **kwargs):
+        return scope.int(
+            scope.hyperopt_param(label,
+                                 scope.qloguniform(*args, **kwargs)))
+
+    def quniform_int(label, *args, **kwargs):
+        return scope.int(
+            scope.hyperopt_param(label,
+                                 scope.quniform(*args, **kwargs)))
+
+
