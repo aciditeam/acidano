f77a1d7c711296b4ff963d7fdab947211c6158f8
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2016-12-14 18:22:57 +0100

    Waveform visualization simplified (too computationnaly expensive)

--- acidano/visualization/waveform/visualize_waveform.py
+++ acidano/visualization/waveform/visualize_waveform.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
 from acidano.visualization.waveform.write_waveform_html import write_waveform_html
 
-def visualize_waveform(waveform, path, file_name_no_extension):
+def visualize_waveform(waveform, path, file_name_no_extension, subsampling_before_plot=1):
+    waveform_subsampled = waveform[0::subsampling_before_plot]
     temp_csv = path + '/' + file_name_no_extension + '.csv'
     with open(temp_csv, 'wb') as f:
         f.write("x,y\n")
-        for x in range(len(waveform)):
-            f.write(str(x) + ',' + str(waveform[x]) + '\n')
+        for x in range(len(waveform_subsampled)):
+            f.write(str(x) + ',' + str(waveform_subsampled[x]) + '\n')
 
     write_waveform_html(path + '/' + file_name_no_extension + ".html", file_name_no_extension)
 
 
 if __name__ == '__main__':
     import numpy as np
-    waveform=np.zeros((5000))
-    for x in range(5000):
-        waveform[x] = math.sin(2 * math.pi * x / 153)
-    visualize_waveform(waveform, '.', 'test')
+
+    wave = np.loadtxt('waveform.csv', delimiter=',')
+    wave = wave[:,1]
+
+    # Call visualize_waveform
+    visualize_waveform(wave, '.', 'test', 1)
