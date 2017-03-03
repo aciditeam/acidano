eb132ed7f98104f14f2b0bcf477f07117b80db67
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2017-03-03 18:13:41 +0100

    Changes in build_data imply changes in unit_type

--- acidano/data_processing/utils/unit_type.py
+++ acidano/data_processing/utils/unit_type.py
@@ -7,20 +7,26 @@ from acidano.data_processing.utils.categorical import from_continuous_to_categor
 import numpy as np
 import re
 
-def type_conversion(matrix, unit_type):
+def type_conversion(dico_matrix, unit_type):
+    result = {}
     # Binary unit ?
     if unit_type == 'binary':
-        matrix[np.nonzero(matrix)] = 1
+        for k,matrix in dico_matrix.iteritems():
+            matrix[np.nonzero(matrix)] = 1
+            result[k] = matrix
     elif unit_type == 'continuous':
         # Much easier to work with unit between 0 and 1, for several reason :
         #       - 'normalized' values
         #       - same reconstruction as for binary units when building midi files
-        matrix = matrix / 127
+        for k,matrix in dico_matrix.iteritems():
+            matrix = matrix / 127
+            result[k] = matrix
     elif re.search('categorical', unit_type):
         # Categorical
-        m = re.search(r'[0-9]+$', unit_type)
-        N_category = int(m.group(0))
-        matrix = matrix / 127
-        matrix = from_continuous_to_categorical(matrix, N_category)
-
-    return matrix
+        for k,matrix in dico_matrix.iteritems():
+            m = re.search(r'[0-9]+$', unit_type)
+            N_category = int(m.group(0))
+            matrix = matrix / 127
+            matrix = from_continuous_to_categorical(matrix, N_category)
+            result[k] = matrix
+    return result
