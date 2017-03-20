4d0e6460c3a6329413ea7821c32edb02c3cfdb07
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2017-03-20 17:09:11 +0100

    First keras model

---
+++ acidano/models/lop_keras/binary/lstm.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from acidano.models.lop_keras.model_lop_keras import Model_lop_keras
+
+# Hyperopt
+from hyperopt import hp
+from acidano.utils import hopt_wrapper
+from math import log
+
+# Keras
+import keras
+from keras.layers import Input, Embedding, LSTM, Dense
+from keras.models import Model
+
+class Lstm(Model_lop_keras):
+    def __init__(self, model_param, dimensions):
+        Model_lop_keras.__init__(self, model_param, dimensions)
+        self.n_hs = model_param['n_hidden']
+        return
+
+    @staticmethod
+    def get_hp_space():
+        super_space = Model_lop_keras.get_hp_space()
+
+        space = {'n_hidden': hp.choice('n_hidden', [
+            [],
+            [hopt_wrapper.qloguniform_int('n_hidden_1_'+str(i), log(100), log(5000), 10) for i in range(1)],
+            [hopt_wrapper.qloguniform_int('n_hidden_2_'+str(i), log(100), log(5000), 10) for i in range(2)],
+            [hopt_wrapper.qloguniform_int('n_hidden_3_'+str(i), log(100), log(5000), 10) for i in range(3)],
+        ]),
+        }
+
+        space.update(super_space)
+        return space
+
+    @staticmethod
+    def get_name():
+        return "lstm_piano_pluged_last"
+
+    def build_model(self):
+        # Main input is a sequence of orchestra vectors
+        main_input = Input(shape=(self.temporal_order, self.orch_dim), name='orch_seq')
+        # Stack lstm
+        if self.n_hs:
+            x = LSTM(self.n_hs[0], return_sequences=True, input_shape=(self.temporal_order, self.orch_dim),
+                dropout=self.dropout_probability,
+                kernel_regularizer=keras.regularizers.l2(self.weight_decay_coeff),
+                bias_regularizer=keras.regularizers.l2(self.weight_decay_coeff))(main_input)
+            for l in range(1, len(self.n_hs)):
+                x = LSTM(self.n_hs[l], return_sequences=True,
+                    dropout=self.dropout_probability,
+                    kernel_regularizer=keras.regularizers.l2(self.weight_decay_coeff),
+                    bias_regularizer=keras.regularizers.l2(self.weight_decay_coeff))(x)
+            # Seems logic to me that end of lstm has the same size as piano piano-roll
+            lstm_out = LSTM(self.piano_dim,
+                dropout=self.dropout_probability,
+                kernel_regularizer=keras.regularizers.l2(self.weight_decay_coeff),
+                bias_regularizer=keras.regularizers.l2(self.weight_decay_coeff))(x)
+        else:
+            lstm_out = LSTM(self.piano_dim, input_shape=(self.temporal_order, self.orch_dim),
+                dropout=self.dropout_probability,
+                kernel_regularizer=keras.regularizers.l2(self.weight_decay_coeff),
+                bias_regularizer=keras.regularizers.l2(self.weight_decay_coeff))(main_input)
+        # Auxiliary input
+        auxiliary_input = Input(shape=(self.piano_dim,), name='piano_t')
+        # Concatenate
+        x = keras.layers.concatenate([lstm_out, auxiliary_input], axis=1)
+        # Dense layers on top
+        orch_prediction = Dense(self.orch_dim, activation='sigmoid', name='orch_pred',
+            kernel_regularizer=keras.regularizers.l2(self.weight_decay_coeff),
+            bias_regularizer=keras.regularizers.l2(self.weight_decay_coeff))(x)
+        model = Model(inputs=[main_input, auxiliary_input], outputs=orch_prediction)
+        # Instanciate the model
+        self.model = model
+        return
+
+    def set_model(self, model):
+        # Used to re-instanciate an already trained model
+        self.model = model
+
+    def fit(self, orch_past, orch_t, piano_past, piano_t):
+        return (self.model).fit(x={'orch_seq': orch_past, 'piano_t': piano_t},
+                                y=orch_t,
+                                epochs=1,
+                                batch_size=self.batch_size,
+                                verbose=0)
+
+
+    def validate(self, orch_past, orch_t, piano_past, piano_t):
+        return (self.model).predict(x={'orch_seq': orch_past, 'piano_t': piano_t},
+                                              batch_size=self.batch_size)
---
+++ acidano/models/lop_keras/model_lop_keras.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+# General remarks about the architecture
+# Model is not part of the object, since we want to save it in a different adapted format
+# Hence the object only conatins "meta information", not the model itself
+
+# Plot lib
+from acidano.utils import hopt_wrapper
+from hyperopt import hp
+from math import log
+
+
+class Model_lop_keras(object):
+    """
+    Template class for the lop keras models
+    """
+    def __init__(self, model_param, dimensions):
+        # Training parameters
+        self.batch_size = model_param['batch_size']
+        self.temporal_order = model_param['temporal_order']
+        # Regularization paramters
+        self.dropout_probability = model_param['dropout_probability']
+        self.weight_decay_coeff = model_param['weight_decay_coeff']
+        # Dimensions
+        self.temporal_order = model_param['temporal_order']
+        self.orch_dim = dimensions['orch_dim']
+        self.piano_dim = dimensions['piano_dim']
+        return
+
+    @staticmethod
+    def get_hp_space():
+        space_training = {'batch_size': hopt_wrapper.quniform_int('batch_size', 50, 500, 1),
+                          'temporal_order': hopt_wrapper.qloguniform_int('temporal_order', log(3), log(20), 1)
+                          }
+        space_regularization = {'dropout_probability': hp.choice('dropout', [
+            0.0,
+            hp.normal('dropout_probability', 0.5, 0.1)
+        ]),
+            'weight_decay_coeff': hp.choice('weight_decay_coeff', [
+                0.0,
+                hp.uniform('a', 1e-4, 1e-2)
+            ])
+        }
+        space_training.update(space_regularization)
+        return space_training
